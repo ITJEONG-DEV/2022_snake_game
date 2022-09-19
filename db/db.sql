bffab569502998e844b1c958af98e5980456f0ce
ITJEONG-DEV <derbana1027@gmail.com>
2022-09-19 15:07:42 +0900

    쿼리 예제 생성 및 db 서버와 연동

--- db/db.sql
+++ db/db.sql
@@ -12,7 +12,35 @@ CREATE TABLE rankdata(
 );
 
 INSERT INTO rankdata (_username, _score, _time) VALUES('itjeong', 30, NOW());
+INSERT INTO rankdata (_username, _score, _time) VALUES('itjeong', 20, NOW());
+INSERT INTO rankdata (_username, _score, _time) VALUES('itjeong', 60, NOW());
+INSERT INTO rankdata (_username, _score, _time) VALUES('bob', 60, NOW());
 
 use snakegame;
 
-select * from rankdata;
+SELECT * FROM rankdata;
+
+SELECT * FROM rankdata ORDER BY _score DESC LIMIT 3;
+
+SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata;
+
+# 상위 5개 점수만 가져옴
+SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata LIMIT 5;
+
+# 1-5등의 점수만 가져옴
+SELECT * FROM (SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata) ranked WHERE ranked._rank<=5;
+
+# 선택한 유저의 점수가 몇 등인지 가져옴
+SELECT * FROM (SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata) ranked WHERE ranked._username = 'itjeong' and ranked._score=60;
+
+# 최대 등수
+SELECT MAX(ranked._rank) FROM (SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata) ranked;
+
+# 전체 인원
+SELECT COUNT(*) _count FROM rankdata;
+
+# 나의 위에 몇 명이 있는지
+SELECT COUNT(*) FROM (SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata) ranked WHERE ranked._score>50;
+
+# 나의 아래에 몇 명이 있는지
+SELECT COUNT(*) FROM (SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata) ranked WHERE ranked._score<50;
--- server/app.js
+++ server/app.js
@@ -1,79 +1,98 @@
 const express = require('express');
 const mysql = require('mysql');
 const fs = require('fs');
+const { rejects } = require('assert');
 
 const SERVER_PORT = 3308;
 
 const app = express();
 
-const sqlConnection = createConnection();
-sqlConnection.connect();
-
-app.get('/', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.send('Hello World!' + req);
-});
-
-// /addrank?name={name}&score={score}&id={id}
-app.get('/addrank', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-
-    var name = req.query.name;
-    var score = req.query.score;
-
-    const query = "INSERT INTO rankdata(_username, _score, _time)" + "VALUES('" + name + "', " + score + ", NOW());";
-
-    sqlConnection.query(query, function(error, results, fields) {
-        if(error) {
-            console.log(error);
-
-            return res.send("error: " + error);
-        }
-        // console.log(results);
-        // console.log(fields);
-
-        return res.send(JSON.stringify({
-            "results":results,
-            "fields":fields
-        }));
-    });
-});
-
-app.get('/getrank', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-
-    const query = "SELECT * FROM rankdata";
-    sqlConnection.query(query, function(error, results, fields) {
-        if(error) {
-            console.log(error);
-
-            return res.send("error: " + error);
-        }
-
-        // console.log(results);
-        // console.log(fields);
-
-        return res.send(JSON.stringify({
-            "results":results,
-            "fields":fields
-        }));
-    });
-});
-
-app.listen(SERVER_PORT, () => {
-    console.log('example app listening at http://localhost:' + SERVER_PORT);
-});
-
-function createConnection() {
+try {
     const jsonFile = fs.readFileSync('./server/dbinfo.json', 'utf-8');
     const jsonData = JSON.parse(jsonFile);
-
-    return mysql.createConnection({
+    
+    const sqlConnection = mysql.createConnection({
         host: jsonData["host"],
         user: jsonData["user"],
         password: jsonData["password"],
         database: jsonData["database"]
     });
-}
 
-// connection.end();
+    sqlConnection.connect(function(e) {
+        if(e) throw e;
+    });
+    
+    // /addrank?name={name}&score={score}&id={id}
+    app.get('/addrank', async (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
+
+        var name = req.query.name;
+        var score = req.query.score;
+    
+        const query1 = "INSERT INTO rankdata(_username, _score, _time)" + "VALUES('" + name + "', " + score + ", NOW());";
+    
+        let data = {};
+
+        Execute(query1)
+        .then((result) => {
+            data["rows"] = result;
+        })
+        .then(() => {
+            return res.send(data);
+        })
+        .catch((e) => {
+            console.error(e);
+        })
+    });
+    
+    app.get('/getrank', async (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
+
+        const limit = 5;
+    
+        var name = req.query.name;
+        var score = req.query.score;
+    
+        const query1 = "SELECT * FROM (SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata) ranked WHERE ranked._rank<=" + limit + ";";
+        const query2 = "SELECT * FROM (SELECT _username, _score, DENSE_RANK() OVER (ORDER BY _score DESC) _rank FROM rankdata) ranked WHERE ranked._username = '"+ name +"' and ranked._score=" + score + ";"
+
+        let data = {};
+
+        const topRank = Execute(query1)
+        .then((result) => {
+            data["topRank"] = result;
+        });
+
+        const myRank = Execute(query2)
+        .then((result) => {
+            data["myRank"] = result;
+        });
+
+        Promise.all([topRank, myRank])
+        .then(() => {
+            return res.send(data);
+        })
+        .catch((e) => {
+            console.error(e);
+        })
+    });
+    
+    app.listen(SERVER_PORT, () => {
+        console.log('example app listening at http://localhost:' + SERVER_PORT);
+    });
+    
+    // connection.end();
+
+    function Execute(query) {
+        return new Promise((resolve, reject) => {
+            sqlConnection.query(query, (error, rows, fields) => {
+                if(error) return reject(error);
+
+                return resolve(rows);
+            });
+        });
+    }
+
+} catch(e) {
+    console.log(e);
+}
